db6b24b914b2c75660b3c368af959ad816e986f7
Philippe <p.lee9519@gmail.com>
2022-12-30 14:58:48 -0500

    Added Marker and Autocomplete now pops up map

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,25 +1,34 @@
 <template>
-  <section class="ui two column centered grid">
-    <div class="column">
-      <form class="ui segment large form" :class="{ loading: spinner }">
-        <div class="ui message red" v-show="error">{{ error }}</div>
-        <div class="ui segement">
-          <div class="field">
-            <div class="ui right icon input large">
-              <input
-                type="text"
-                placeholder="Enter Your Address"
-                v-model="address"
-                id="autocomplete"
-              />
-              <i class="dot circle icon link" @click="locatorButtonPressed"></i>
+  <div>
+    <section
+      class="ui two column centered grid"
+      style="position:relative; z-index: 1;"
+    >
+      <div class="column">
+        <form class="ui segment large form" :class="{ loading: spinner }">
+          <div class="ui message red" v-show="error">{{ error }}</div>
+          <div class="ui segement">
+            <div class="field">
+              <div class="ui right icon input large">
+                <input
+                  type="text"
+                  placeholder="Enter Your Address"
+                  v-model="address"
+                  id="autocomplete"
+                />
+                <i
+                  class="dot circle icon link"
+                  @click="locatorButtonPressed"
+                ></i>
+              </div>
             </div>
+            <button class="ui button">Go</button>
           </div>
-          <button class="ui button">Go</button>
-        </div>
-      </form>
-    </div>
-  </section>
+        </form>
+      </div>
+    </section>
+    <section id="map"></section>
+  </div>
 </template>
 
 <script>
@@ -33,7 +42,7 @@ export default {
     };
   },
   mounted() {
-    new google.maps.places.Autocomplete(
+    let autocomplete = new google.maps.places.Autocomplete(
       document.getElementById("autocomplete"),
       {
         bounds: new google.maps.LatLngBounds(
@@ -41,6 +50,13 @@ export default {
         )
       }
     );
+    autocomplete.addListener("place_changed", () => {
+      let place = autocomplete.getPlace();
+      this.showUserLocationOnMap(
+        place.geometry.location.lat(),
+        place.geometry.location.lng()
+      );
+    });
   },
   methods: {
     locatorButtonPressed() {
@@ -49,6 +65,10 @@ export default {
         navigator.geolocation.getCurrentPosition(
           position => {
             this.getAddressFrom(
+              position.coords.latitude,
+              position.coords.longitude
+            );
+            this.showUserLocationOnMap(
               position.coords.latitude,
               position.coords.longitude
             );
@@ -67,7 +87,7 @@ export default {
       console.log(process.env.GOOGLE_API_KEY);
       axios
         .get(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${VUE_APP_GOOGLE_API_KEY}`
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.VUE_APP_GOOGLE_API_KEY}`
         )
         .then(response => {
           if (response.data.error_message) {
@@ -83,6 +103,17 @@ export default {
           this.error = error.message;
           console.log(error.message);
         });
+    },
+    showUserLocationOnMap(lat, long) {
+      let map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: new google.maps.LatLng(lat, long),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      new google.maps.Marker({
+        position: new google.maps.LatLng(lat, long),
+        map: map
+      });
     }
   }
 };
@@ -106,5 +137,14 @@ export default {
 }
 .pac-icon {
   display: none;
+}
+
+#map {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: red;
 }
 </style>
